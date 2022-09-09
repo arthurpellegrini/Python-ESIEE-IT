82657c94b3dcf43432844d7c01eabe0fa828eb51
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2022-09-09 15:48:58 +0200

    Refactor

---
+++ 08-09-2022/ELECTRICITE.py
@@ -0,0 +1,22 @@
+def programme(conso: float) -> str:
+    """
+    Cette fonction permet de déterminer le programme le plus adéquat en fonction de la consommation du client
+    :param conso: la consommation du client
+    :return: une chaîne de caractères contenant le programme correspondant
+    """
+    if conso < 3:
+        return "5,74€/mois + 15,55€/KWh"
+    elif conso < 6:
+        return "8,92€/mois + 14,67€/KWh"
+    elif conso < 9:
+        return "10,42€/mois + 14,83€/KWh"
+    elif conso < 12:
+        return "11,96€/mois + 14,83€/KWh"
+    elif conso < 15:
+        return "13,50€/mois + 14,83€/KWh"
+
+
+# --------------------------------MAIN--------------------------------#
+if __name__ == '__main__':
+    conso = float(input("Combien consommez-vous ? : "))
+    print("Le prix de l'abonnement sera de %s" % programme(conso))
