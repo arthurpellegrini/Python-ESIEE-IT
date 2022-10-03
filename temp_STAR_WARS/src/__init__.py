1d7844b41138c5fbbb1aa30ac60d67eea81a2f3e
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2022-10-03 16:38:01 +0200

    Delete temp_STAR_WARS directory

--- temp_STAR_WARS/src/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from .Film import Film
-from .Acteur import Acteur
-from .Personnage import Personnage
-from .Gentil import Gentil
-from .Mechant import Mechant
