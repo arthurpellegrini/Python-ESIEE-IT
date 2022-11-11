9da2250e4139cbaf3017b9235b61c78552907ef2
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2022-11-11 20:12:15 +0100

    Co-authored-by: Clem-0000 <clemtoon78700.bri@gmail.com>

--- BigBlueButton/main.py
+++ BigBlueButton/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pandas.core.series as s
 
 cols_name = ["Date", "Heure", "Data"]
 dataBB = pd.read_fwf("dataBB.log.txt", encoding="latin-1", usecols=[0, 1, 5], names=cols_name)
@@ -8,6 +7,7 @@ print(dataBB, dataBB.shape)
 
 for i in dataBB['Data'].index:
     if "method=" in dataBB['Data'][i]:
+        pass
         # dataBB = dataBB.drop(dataBB['Data'][i], inplace=True)
 
 
