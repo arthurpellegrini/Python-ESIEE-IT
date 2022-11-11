b7da58458509903df0f7a784a71843a716192433
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2022-11-11 21:16:44 +0100

    feat: BigBlueButton - Nettoyage des données
    Co-authored-by: Clem-0000 <clemtoon78700.bri@gmail.com>

--- BigBlueButton/main.py
+++ BigBlueButton/main.py
@@ -1,14 +1,24 @@
 import pandas as pd
-
-cols_name = ["Date", "Heure", "Data"]
-dataBB = pd.read_fwf("dataBB.log.txt", encoding="latin-1", usecols=[0, 1, 5], names=cols_name)
-dataBB = dataBB.drop_duplicates()
-print(dataBB, dataBB.shape)
-
-for i in dataBB['Data'].index:
-    if "method=" in dataBB['Data'][i]:
-        pass
-        # dataBB = dataBB.drop(dataBB['Data'][i], inplace=True)
+from pandas import *
 
 
-print(dataBB, dataBB.shape)
+def get_data() -> DataFrame:
+    cols_name = ["Date", "Heure", "Data"]
+    data = pd.read_fwf("dataBB.log.txt", encoding="latin-1", usecols=[0, 1, 5], names=cols_name)
+    data = data.drop_duplicates()
+
+    # On enlève les données qui contiennent des method=GET ou method=PUSH car pas utiles pour le travail à effectuer.
+    indexes = []
+    for i in data['Data'].index:
+        if "method=" in data['Data'][i]:
+            indexes.append(i)
+
+    data = data.drop(indexes)
+
+    return data
+
+
+if __name__ == "__main__":
+    dataBB = get_data()
+    print(dataBB, dataBB.shape)
+
