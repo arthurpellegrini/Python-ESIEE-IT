a5185ac69034c945510a5b1ea9f44f1e84d2669d
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2022-11-23 09:00:16 +0100

    Refactor Class

--- BigBlueButton/main.py
+++ BigBlueButton/main.py
@@ -2,31 +2,32 @@ import pandas as pd
 from pandas import *
 
 
-def get_data() -> DataFrame:
-    cols_name = ["Date", "Heure", "Data"]
-    data = pd.read_fwf("dataBB.log.txt", encoding="latin-1", usecols=[0, 1, 5], names=cols_name)
-    data = data.drop_duplicates()
+class DataBB:
+    def __init__(self):
+        self.data = None
 
-    # On enlève les données qui contiennent des method=GET ou method=PUSH car pas utiles pour le travail à effectuer.
-    indexes = []
-    for i in data['Data'].index:
-        raw_data = data['Data'][i]
-        if "method=" in raw_data:
-            indexes.append(i)
-        #PAS TERMINé à revoir
-        elif "[ActiveJob]" or "[NotifyUserWaitingJob]" in raw_data:
-            print("BEFORE", raw_data)
-            raw_data = raw_data.replace("[ActiveJob]", "").replace("[NotifyUserWaitingJob]", "")
-            print("AFTER", raw_data)
-            data.replace(data['Data'][i], raw_data)
+    def get_data(self, filename: str) -> DataFrame:
+        """
+        Cette fonction permet le prétraitement des données qui sont reçus en paramètres.
+        """
+        self.data = pd.read_fwf(filename, encoding="latin-1", usecols=[0, 1, 5], names=["Date", "Heure", "Data"])
+        self.data = self.data.drop_duplicates()
 
-    data = data.drop(indexes)
+        indexes = []
+        for i in self.data['Data'].index:
+            if "method=" in self.data['Data'][i]:
+                indexes.append(i)
+        return self.data.drop(indexes)
 
-    # Créer de nouvelles colonnes pour les ID
+    def details_connexion_mail(self, mail: str) -> DataFrame:
+        pass
 
-    return data
+    def details_connexion_room(self, room: str):
+        pass
 
 
 if __name__ == "__main__":
-    dataBB = get_data()
-    print(dataBB, dataBB.shape)
+    dataBB = DataBB()
+    dataBB.get_data("dataBB.log.txt")
+
+    print(dataBB.data, dataBB.data.shape)
